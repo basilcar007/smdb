9cb2d6d5af70cab811120c6efe7356b595eb6728
Vassilis Karadimas <karadimas.vasilios@nbg.gr>
2021-05-19 09:39:55 +0300

    Test commit

--- src/main/java/nbg/spring/smdb/SmdbApplication.java
+++ src/main/java/nbg/spring/smdb/SmdbApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmdbApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SmdbApplication.class, args);
+        // test commit
+
     }
 
 }
